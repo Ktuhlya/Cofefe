085acf4edba20dbd11f29c64c6b77ded9460f71a
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-01-30 15:00:27 +0700

    123

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -222,50 +222,6 @@ fun buy() {
 }
 
 
+class coffeeMachine () {
 
-/*
-
-  val list = mutableListOf<Int>()
-    print("Write how many ml of water the coffee machine has: > ")
-    val waterIn =list.add(readln().toInt() / 200)
-    print("Write how many mj of milk the coffee machine has: > ")
-    val milkIn = list.add(readln().toInt() / 50)
-    print("Write how many grams of coffee beans the coffee machine has: > ")
-    val coffeeIn = list.add(readln().toInt() / 15)
-    print("Write how many cups of coffee you will need: > ")
-    val cupsWanted = readln().toInt()
-    val maxCupsIn = list.minOrNull()
-   if (maxCupsIn!! < cupsWanted) {
-       println("No, I can make only $maxCupsIn cups of coffee")
-   }
-    if (maxCupsIn == cupsWanted) {
-        println("Yes, I can make that amount of coffee")
-    }
-    if (maxCupsIn > cupsWanted) {
-        println("Yes, I can make that amount of coffee " +
-                "(and even ${maxCupsIn - cupsWanted} more than that")
-
-
-//////////
-
-println("Write how many cups of coffee you will need: > ")
-    val cups = readln().toInt()
-    println("For $cup of coffee you will need:")
-    println("${cup*200} ml of water" )
-    println("${cup*50} ml of milk")
-    println("${cup*15} g of coffee beans")
-///
- println("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!
-    """.trimIndent()
-    )
-
-
-
- */
+}

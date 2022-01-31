ed51ee1604b27e071bbb4ef61f488ec6d9c43744
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-01-31 14:53:31 +0700

    1234

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,227 +1,122 @@
 package machine
 
-import kotlin.system.exitProcess
-
-
-var waterIn = 400
-var milkIn = 540
-var coffeeIn = 120
-var dispCups = 9
-var moneyIn = 550
-
-var list = mutableListOf<Int>(waterIn, milkIn, coffeeIn)
+enum class KindCoffee (val kind: String, val money: Int, val water:Int,
+                       val milk:Int, val coffee: Int) {
+    ESPRESSO("1", 4, 250, 0, 16),
+    LATTE("2", 7, 350, 75, 20),
+    CAPPUCCINO("3", 6, 200, 100, 12),
+}
 
 fun main() {
 
-
-   actionMenu()
-
+    actionMenu()
 
     }
 
-fun machineState() {
-    println("")
-    println("The coffee machine has:")
-    println("$waterIn of water")
-    println("$milkIn of milk")
-    println("$coffeeIn of coffee beans")
-    println("$dispCups of disposable cups")
-    println("$$moneyIn of money")
-    println("")
-    actionMenu()
-
-
-
-}
-
 fun actionMenu() {
+
     print("Write action (buy, fill, take, remaining, exit): ")
     when (readln()) {
-        "buy" -> buy()
-        "fill" -> fillF()
-        "take" -> takeF()
-        "remaining" -> machineState()
+        "buy" -> {
+            coffeeMachine(state = MachineState.State.state ).buy()
+        }
+        "fill" -> {
+            coffeeMachine(MachineState.State.state ).fillF()
+        }
+        "take" -> {
+            coffeeMachine(MachineState.State.state ).takeF()
+        }
+        "remaining" -> {
+            coffeeMachine(MachineState.State.state ).machineState()
+        }
         "exit" -> System.exit(0)
     }
 
 }
 
-fun takeF() {
-    println("")
-   println("I gave you $$moneyIn")
-    moneyIn = 0
 
-    println()
-   actionMenu()
+class  MachineState(var waterIn: Int, var milkIn: Int, var coffeeIn: Int,
+                    var dispCups: Int, var moneyIn: Int){
+    object State {
+        var state: MachineState = MachineState(400, 540, 120, 9, 550)
+
+    }
 }
 
-fun fillF() {
-    println("")
-     print("Write how many ml of water do you want to add: ")
-    waterIn += readln().toInt()
-     print("Write how many ml of milk do you want to add: ")
-    milkIn += readln().toInt()
-     print("Write how many grams of coffee beans do you want to add: ")
-    coffeeIn += readln().toInt()
-     print("Write how many disposable cups of coffee " +
-             "do you want to add: ")
-    dispCups += readln().toInt()
 
-    list = mutableListOf<Int>(waterIn, milkIn, coffeeIn)
-    println("")
-    actionMenu()
-}
+class coffeeMachine (var state : MachineState) {
 
-fun buy() {
-    println("")
-    print(
-        "What do you want to buy? 1 - espresso, " + "2 - latte, " +
-                "3 - cappuccino, back - to main menu: "
-    )
-    when (readln()) {
+    fun buy(){
+        println("")
+        print(
+            "What do you want to buy? 1 - espresso, " + "2 - latte, " +
+                    "3 - cappuccino, back - to main menu: "
+        )
+        var indCoffee = readln()
+        if (indCoffee == "back") actionMenu()
+        var kindCoffee: KindCoffee = KindCoffee.values()[indCoffee.toInt()-1]
 
-        "1" -> {
-            moneyIn += 4
-            waterIn -= 250
-            coffeeIn -= 16
-            dispCups -= 1
-            if (waterIn < 0){
-                println("Sorry, not enough water!")
-                moneyIn -= 4
-                waterIn += 250
-                coffeeIn += 16
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (milkIn < 0){
-                println("Sorry, not enough milk!")
-                moneyIn -= 4
-                waterIn += 250
-                coffeeIn += 16
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (dispCups < 0) {
-                println("Sorry, not enough disposable cups!")
-                moneyIn -= 4
-                waterIn += 250
-                coffeeIn += 16
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-        }
-        "2" -> {
-            moneyIn += 7
-            waterIn -= 350
-            milkIn -= 75
-            coffeeIn -= 20
-            dispCups -= 1
-            if (waterIn < 0){
-                println("Sorry, not enough water!")
-                moneyIn -= 7
-                waterIn += 350
-                milkIn += 75
-                coffeeIn += 20
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (milkIn < 0){
-                println("Sorry, not enough milk!")
-                moneyIn -= 7
-                waterIn += 350
-                milkIn += 75
-                coffeeIn += 20
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (coffeeIn < 0){
-                println("Sorry, not enough coffee beans!")
-                moneyIn -= 7
-                waterIn += 350
-                milkIn += 75
-                coffeeIn += 20
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (dispCups < 0) {
-                println("Sorry, not enough disposable cups!")
-                moneyIn -= 7
-                waterIn += 350
-                milkIn += 75
-                coffeeIn += 20
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-        }
-        "3" -> {
-            moneyIn += 6
-            waterIn -= 200
-            milkIn -= 100
-            coffeeIn -= 12
-            dispCups -= 1
-            if (waterIn < 0){
-                println("Sorry, not enough water!")
-                moneyIn -= 6
-                waterIn += 200
-                milkIn += 100
-                coffeeIn += 12
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (milkIn < 0){
-                println("Sorry, not enough milk!")
-                moneyIn -= 6
-                waterIn += 200
-                milkIn += 100
-                coffeeIn += 12
-                dispCups += 1
-                println("")
-                actionMenu()
+        when {
+            state.waterIn < kindCoffee.water -> println("Sorry, not enough water!")
+            state.milkIn < kindCoffee.milk -> println("Sorry, not enough milk!")
+            state.coffeeIn < kindCoffee.coffee -> println("Sorry, not coffee beans!")
+             state.dispCups < 1 -> println("Sorry, not enough disposable cups!")
+            else -> {
+              state.waterIn = state.waterIn- kindCoffee.water
+              state.coffeeIn = state.coffeeIn - kindCoffee.coffee
+              state.milkIn = state.milkIn - kindCoffee.milk
+              state.moneyIn = state.moneyIn + kindCoffee.money
+               state.dispCups --
+                println("I have enough resources, making you a coffee!")
+                println()
 
             }
-            if (coffeeIn < 0){
-                println("Sorry, not enough coffee beans!")
-                moneyIn -= 6
-                waterIn += 200
-                milkIn += 100
-                coffeeIn += 12
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
-            if (dispCups < 0) {
-                println("Sorry, not enough disposable cups!")
-                moneyIn -= 6
-                waterIn += 200
-                milkIn += 100
-                coffeeIn += 12
-                dispCups += 1
-                println("")
-                actionMenu()
-            }
         }
-        "back" -> {
-            println("")
-            actionMenu()
-        }
+
+        actionMenu()
 
     }
 
-    list = mutableListOf<Int>(waterIn, milkIn, coffeeIn)
-    println("I have enough resources, making you a coffee!")
-    println()
-  actionMenu()
+    fun fillF() {
+        println("")
+        print("Write how many ml of water do you want to add: ")
+       state.waterIn += readln().toInt()
+        print("Write how many ml of milk do you want to add: ")
+        state.milkIn += readln().toInt()
+        print("Write how many grams of coffee beans do you want to add: ")
+        state.coffeeIn += readln().toInt()
+        print("Write how many disposable cups of coffee " +
+                "do you want to add: ")
+        state.dispCups += readln().toInt()
+
+        println("")
+        actionMenu()
+    }
+
+    fun takeF() {
+        println("")
+        println("I gave you $${state.moneyIn}")
+        state.moneyIn = 0
+
+        println()
+        actionMenu()
+    }
+
+    fun machineState() {
+
+        println("")
+        println("The coffee machine has:")
+        println("${state.waterIn} of water")
+        println("${state.milkIn} of milk")
+        println("${state.coffeeIn} of coffee beans")
+        println("${state.dispCups} of disposable cups")
+        println("$${state.moneyIn} of money")
+        println("")
+        actionMenu()
+
+
+    }
 }
 
 
-class coffeeMachine () {
 
-}
